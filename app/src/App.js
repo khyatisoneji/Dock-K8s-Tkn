111b07ed43d79da2f4a7b6c4148941f84be45d85
khyatisoneji <31898080+khyatisoneji@users.noreply.github.com>
2021-02-17 15:19:13 +0530

    Added post request to add data into database

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { config } from "process";
  
 class App extends Component {
   constructor(props){
               super(props);
               this.state = {
                      data: [],
+		     inputValue: '',
                      isLoading: false,
                };
-               this.click = this.click.bind(this);
+               this.add = this.add.bind(this);
+	       this.show = this.show.bind(this);
+               this.handleChange = this.handleChange.bind(this);
         }
 
-    click() {
+    handleChange(event) {
+      this.setState({inputValue: event.target.value});
+    }
+
+    async add(e) {
+	    e.preventDefault();
+            this.setState({ data: [] });
+	    this.setState({ isLoading: true });
+
+	    const headers = {
+		'Content-Type':'application/json'
+	    };
+
+	    await axios.post(
+		"welcome", 
+		 {
+		    text: this.state.inputValue,
+		},
+		{headers}
+		).then(response => {
+		    console.log("Success ========>", response);
+		})
+		.catch(error => {
+		    console.log("Error ========>", error);
+		}
+	    )
+            this.setState({ inputValue: '' });
+	    this.setState({ isLoading: false });
+}
+
+    show() {
         this.setState({ isLoading: true });
 
-        axios.get(`/welcome`, {})
+        axios.get("/welcome", {})
             .then((response) => {
                   console.log("response: " + response)
                   this.setState({ data: response.data, isLoading: false });
+		  <p>{this.state.data}</p>
              })
             .catch((err) => {
                   this.setState({ data: err, isLoading: false });
@@ -27,8 +62,11 @@ class App extends Component {
     render() {
        return  (
             <div>
-                <button onClick={this.click} disabled={this.state.isLoading}> click me </button>
-                {this.state.data[0]}
+                <form>
+		  <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+                  <button onClick={this.add} disabled={this.state.isLoading}> Add </button>
+	          <button onClick={this.show} disabled={this.state.isLoading}> Show all </button>
+                </form>
             </div>
            );
     }
